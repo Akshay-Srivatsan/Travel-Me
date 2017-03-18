0ef1d301e1ecdbbccd2926d3eaec6b31699ad303
Maya Donato <maya.donato@menloschool.org>
2017-03-18 04:20:14 -0700

    added more test questions

--- src/pages/matching/test.js
+++ src/pages/matching/test.js
@@ -1,10 +1,33 @@
 {
   "General Phrases": {
     'What is the word for "Hello"?': ['hola', 'salve', 'bonjour', 'hi'],
+    'What is the word for "Thank you"?' : ['gracias', 'lo siento', 'cómo', 'porque'],
+    'How do you say "Excuse me"?' : ['perdón','si','llamo', 'cercano'],
+    'How do you ask "Where is the bathroom"?' : ['dónde está el baño', 'necesitas algo','por favor','buenos días' ],
+    'How do you say "I need a telephone"?' : ['yo necesito el teléphono.','dame comida','tengo hambre','Es el amanecer' ],
+    'How do you say "Yes"?':['si', 'oui','hai','yes' ],
+    'How do you say "No"?': ['no','non','pero','y'],
+    'What is the word for "Sorry"?':['Lo siento','Pero','Siempre','Nunca'],
+    'How do you ask "Do you speak English"?':['Hablas tú inglés', 'Hablas tú americano', 'Como se dice', 'Porque ases eso'],
+    'What is the word for "Please"?':['Por favor', 'Sí tu quieres','Claro','Dormir'],
   },
   "Cost/Money": {
+    'How do you ask "How much is this?"?':['Cuanto cuesta esto?','Cuál es la modena?', 'Como te llamas?','Cuantos años tienes'],
+    'How do you ask "Do you accept a debit card"?':['Aceptan carta de débito?','A cuanto está el dolar?','Dónde está el banco?','Cual es la hora'],
+    'How do you ask "Where is the bank"?':['Dónde está el banco?','Dónde está la casa?', 'Dónde vas a la escuela','A cuanto está el dolar?'],
+    'What is the word for "Money"?':['Dinero','Pesos','Oro','Modena'],
+    'How do you say "I would like to withdraw money"?':['Quiero retirar dinero.','Dónde está mi dinero?', 'Cuanto dinero tengo?','Que es dinero'],
+    'How do you ask "What is the currency"?':['Cual es la modena?','Cuanto cuesta esto?','Tienen espacio?','Cuantas modenas tienes?'],
+    'What is the word for "Check"?':['Cheque','Carta','Papel','Cuenta'],
+    'How do you say "Credit card"?'['Carta de crédito','Cree en mi','Targeta de papel','Hoja de manejo'],
+    'How do you say "Seventy"?':['Setenta', 'Sesenta','Seis','Siete'],
+    'How do you say "Cash"?':['Effectivo', 'Dinero', 'Débito','Cuantó'],
   },
   "Directions": {
+    'How do you say "West"?':['El oeste','El este','La calle','El tren'],
+    'How do you say "Left"?':['Izquierda','Derecha','Fin','Llegada'],
+    'How do you ask "Where can I buy a plane ticket"?':['Dónde puedo comprar un billete de avión?','Tienes miedo a volar?','Que es un avión?','Dondé está el avión?'],
+    'What is the word for "Train station"?':['La estación de tren','El supermercado','El tren está aquí','']
   },
   "Time": {
   },
