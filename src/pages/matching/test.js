426fccb5e7f1cb700c6b93f5ca7839d2f34eeffa
Maya Donato <maya.donato@menloschool.org>
2017-03-18 04:18:25 -0700

    added test questions

---
+++ src/pages/matching/test.js
@@ -0,0 +1,19 @@
+{
+  "General Phrases": {
+    'What is the word for "Hello"?': ['hola', 'salve', 'bonjour', 'hi'],
+  },
+  "Cost/Money": {
+  },
+  "Directions": {
+  },
+  "Time": {
+  },
+  "Emergencies/Medical": {
+  },
+  "Food": {
+  },
+  "Relationships": {
+  },
+  "Possessions": {
+  }
+}
