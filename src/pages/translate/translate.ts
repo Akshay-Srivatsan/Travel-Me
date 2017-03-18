e9062be722cc3f65f6872ffa0efa6fe829c69a1c
Maya Donato <maya.donato@menloschool.org>
2017-03-18 08:13:35 -0700

    begin Watson

--- src/pages/translate/translate.ts
+++ src/pages/translate/translate.ts
@@ -13,4 +13,21 @@ export class TranslatePage {
     console.log("Translate");
   }
 
+  translate(text) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(xhr.responseText);
+      }
+    }
+    xhr.open('GET', '/api');
+    var username = "6051df59-03cc-4ef8-afab-52f3b1f4145c";
+    var password = "jn6OpyKtyR3h";
+
+
+    var params = JSON.stringify({ source: "en", target: "es", text: text });
+    xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password))
+    xhr.send(params)
+  }
+
 }
